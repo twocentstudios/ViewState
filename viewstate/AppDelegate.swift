aa3f12416d8d8ea3ddad8ecf893157a259847d8e
Christopher Trott <chris@twocentstudios.com>
2017-08-06 23:50:09 -0500

    Remove storyboard

--- viewstate/AppDelegate.swift
+++ viewstate/AppDelegate.swift
@@ -15,7 +15,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        let rootViewController = UIViewController()
+        
+        self.window = UIWindow(frame: UIScreen.main.bounds)
+        self.window?.rootViewController = rootViewController
+        
+        self.window?.makeKeyAndVisible()
+        
         return true
     }
 
