c7f68aea2b785daba43053a8296a1c017e1c62f4
Christopher Trott <chris@twocentstudios.com>
2017-08-08 23:05:21 -0500

    UserInteractor

--- viewstate/UserInteractor.swift
+++ viewstate/UserInteractor.swift
@@ -7,4 +7,72 @@
 //
 
 import Foundation
+import ReactiveSwift
+import Result
 
+final class UserInteractor {
+    enum Command {
+        case loadProfile
+        case loadPosts
+    }
+    
+    enum Effect { }
+    
+    let commandSink: Signal<Command, NoError>.Observer
+    private let commandSignal: Signal<Command, NoError>
+    
+    let viewModel: Property<UserViewModel>
+    let effect: Signal<Effect, NoError>
+    
+    let userId: Int
+    
+    init(userId: Int, profileService: ProfileServiceType, postsService: PostsServiceType, scheduler: SchedulerContext = SchedulerContext()) {
+        self.userId = userId
+        
+        (self.commandSignal, self.commandSink) = Signal<Command, NoError>.pipe()
+        
+        let profileInteractor = ProfileInteractor(userId: userId, service: profileService)
+        let postsInteractor = PostsInteractor(userId: userId, service: postsService)
+        
+        let initialViewModel = UserViewModel(profileViewModel: profileInteractor.viewModel.value, postsViewModel: postsInteractor.viewModel.value)
+        let viewModelSignal = Signal
+            .combineLatest(profileInteractor.viewModel.signal, postsInteractor.viewModel.signal)
+            .map(UserViewModel.init)
+        
+        viewModel = Property(initial: initialViewModel, then: viewModelSignal)
+        
+        commandSignal.map(ProfileInteractor.fromCommand).skipNil().observe(profileInteractor.commandSink)
+        commandSignal.map(PostsInteractor.fromCommand).skipNil().observe(postsInteractor.commandSink)
+        
+        effect = Signal.merge([
+            profileInteractor.effect.map(UserInteractor.toEffect).skipNil(),
+            postsInteractor.effect.map(UserInteractor.toEffect).skipNil()
+            ])
+    }
+    
+    static func toEffect(_ effect: ProfileInteractor.Effect) -> Effect? {
+        return nil
+    }
+    
+    static func toEffect(_ effect: PostsInteractor.Effect) -> Effect? {
+        return nil
+    }
+}
+
+extension ProfileInteractor {
+    static func fromCommand(_ command: UserInteractor.Command) -> Command? {
+        switch command {
+        case .loadProfile: return .load
+        case .loadPosts: return nil
+        }
+    }
+}
+
+extension PostsInteractor {
+    static func fromCommand(_ command: UserInteractor.Command) -> Command? {
+        switch command {
+        case .loadProfile: return nil
+        case .loadPosts: return .load
+        }
+    }
+}
