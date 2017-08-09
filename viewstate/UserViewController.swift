928305d21990b14a301ff45356ae8095399ac635
Christopher Trott <chris@twocentstudios.com>
2017-08-08 23:08:01 -0500

    Create UserViewController

---
+++ viewstate/UserViewController.swift
@@ -0,0 +1,26 @@
+//
+//  UserViewController.swift
+//  viewstate
+//
+//  Created by Christopher Trott on 8/8/17.
+//  Copyright © 2017 twocentstudios. All rights reserved.
+//
+
+import UIKit
+import ReactiveSwift
+import Result
+
+class UserViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}
+
