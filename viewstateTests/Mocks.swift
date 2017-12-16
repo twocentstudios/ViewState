e61abcbb5be7275067fe1041fd63908ff3293b72
Christopher Trott <chris@twocentstudios.com>
2017-12-16 23:52:31 +0900

    mock profileservice and testscheduler

--- viewstateTests/Mocks.swift
+++ viewstateTests/Mocks.swift
@@ -7,6 +7,8 @@
 //
 
 import Foundation
+import ReactiveSwift
+import Result
 @testable import viewstate
 
 enum Mocks {
@@ -19,4 +21,53 @@ enum Mocks {
                            website: URL(string: "twocentstudios.com")!)
     
     static let error = NSError(domain: "", code: 0, userInfo: nil)
+    
+    final class ProfileService: ProfileServiceType {
+        
+        let result: Result<User, NSError>
+        
+        init(_ user: User) {
+            self.result = Result(value: user)
+        }
+        
+        init(_ error: NSError) {
+            self.result = Result(error: error)
+        }
+        
+        func readProfile(userId: Int) -> SignalProducer<User, NSError> {
+            return SignalProducer(result: result)
+        }
+    }
+}
+
+struct TestSchedulerContext: SchedulerContextType {
+    
+    var state: Scheduler {
+        return testState
+    }
+    var work: Scheduler {
+        return testWork
+    }
+    var output: Scheduler {
+        return testOutput
+    }
+    
+    private let testState: TestScheduler
+    private let testWork: TestScheduler
+    private let testOutput: TestScheduler
+    
+    init(state: TestScheduler = TestScheduler(), work: TestScheduler = TestScheduler(), output: TestScheduler = TestScheduler()) {
+        self.testState = state
+        self.testWork = work
+        self.testOutput = output
+    }
+    
+    func nextOutput() {
+        testState.advance()
+        testOutput.advance()
+    }
+    
+    func doWork() {
+        testWork.advance()
+    }
 }
