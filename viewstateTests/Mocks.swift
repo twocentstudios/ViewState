50d1c561ea806b9735bb411b470cef82980c1349
Christopher Trott <chris@twocentstudios.com>
2017-12-16 20:14:03 +0900

    Add mocks

---
+++ viewstateTests/Mocks.swift
@@ -0,0 +1,20 @@
+//
+//  Mocks.swift
+//  viewstateTests
+//
+//  Created by Christopher Trott on 12/16/17.
+//  Copyright © 2017 twocentstudios. All rights reserved.
+//
+
+import Foundation
+@testable import viewstate
+
+enum Mocks {
+    
+    static let user = User(id: 0,
+                           avatarURL: URL(string: "https://en.gravatar.com/userimage/30721452/beb8f097031268cc19d5e6261603d419.jpeg")!,
+                           username: "twocentstudios",
+                           friendsCount: 100,
+                           location: "Chicago",
+                           website: URL(string: "twocentstudios.com")!)
+}
